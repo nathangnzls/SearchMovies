bb2276ef5f228f608475d5d6c403aafc291bfa51
Nathaniel Gonzales <nathanielgonzalesj@gmail.com>
2019-11-11 09:20:58 +0800

    Initial Commit

---
+++ SearchMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchMovies
+//
+//  Created by Nathan on 11/11/2019.
+//  Copyright © 2019 NJG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
